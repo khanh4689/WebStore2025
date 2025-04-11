8c3c25ac10b2918e880610cd8fe8099c1faed6cb
kyorer <khanhtnts00424@fpt.edu.vn>
2025-04-11 20:42:59 +0700

    Ghi chú commit ngắn gọn

--- src/main/java/com/fpoly/Lab6Store1Application.java
+++ src/main/java/com/fpoly/Lab6Store1Application.java
@@ -3,10 +3,13 @@ package com.fpoly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fpoly.config.DotenvConfig;
+
 @SpringBootApplication
 public class Lab6Store1Application {
 
 	public static void main(String[] args) {
+		 new DotenvConfig();
 		SpringApplication.run(Lab6Store1Application.class, args);
 	}
 
---
+++ src/main/java/com/fpoly/config/DotenvConfig.java
@@ -0,0 +1,14 @@
+package com.fpoly.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class DotenvConfig {
+    static {
+        Dotenv dotenv = Dotenv.configure()
+                              .ignoreIfMissing()
+                              .load();
+
+        dotenv.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue()));
+    }
+}
